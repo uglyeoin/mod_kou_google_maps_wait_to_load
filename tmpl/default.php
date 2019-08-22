6b07881ea8824188822d4390eb0bda79b63837de
Eoin Oliver <eoin@squareballoon.co.uk>
2019-08-22 18:43:41 +0100

    update with a commit so I can push

--- tmpl/default.php
+++ tmpl/default.php
@@ -32,16 +32,15 @@ if (file_exists($mediaUrl . '/js/' . $moduleName . ".js") && filesize($mediaUrl 
 
 
 // Access to module parameters
-$dynamic_map            = $params->get('dynamic_map_url');
-$dynamic_map_zoom       = $params->get('dynamic_map_zoom');
-$static_map_place       = preg_replace(" ", "+", $params->get('static_map_place'));
-$static_map_zoom        = $params->get('static_map_zoom');
-$static_map_width        = $params->get('static_map_width');
-$static_map_height        = $params->get('static_map_height');
-$googleMapsStaticApiKey = $params->get('google_maps_static_api_key');
+$dynamic_map            			= $params->get('dynamic_map_url');
+$dynamic_map_zoom       			= $params->get('dynamic_map_zoom');
+$static_map_place       			= $params->get('static_map_place');
+$static_map_zoom        			= $params->get('static_map_zoom');
+$googleMapsStaticApiKey 			= $params->get('google_maps_static_api_key');
+$googleMapsStaticUrlSigningSecret	= $params->get('google_maps_static_url_signing_secret');
 ?>
 
-<span class="mod_google_maps_wait_to_load--span"><?php echo JText::sprintf('MOD_GOOGLE_MAP_CLICK_MAP_TO_ACTIVATE'); ?></span>
-<div class="<?php echo $moduleName ?>" id="mod_google_maps_wait_to_load_container" data-iframe-width="600" data-iframe-height="300" data-iframe-src="<?php echo $dynamic_map . '&z=' . $dynamic_map_zoom; ?>">
-    <a href="#mod_google_maps_wait_to_load_link" title="Click to activate map"><img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $static_map_place; ?>&zoom=<?php echo $static_map_zoom; ?>&scale=1&size=<?php echo $static_map_width; ?>x<?php echo $static_map_height; ?>&maptype=roadmap&format=jpg&visual_refresh=true&key=<?php echo $googleMapsStaticApiKey; ?>" alt="Click to activate map"></a>
+<span>Click to activate map</span>
+<div class="<?php echo $moduleName ?>" id="my-fast-map" data-iframe-width="600" data-iframe-height="300" data-iframe-src="<?php echo $dynamic_map . '&z=' . $dynamic_map_zoom; ?>">
+    <a href="#g-footer" title="Click to activate map"><img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $static_map_place; ?>&zoom=<?php echo $static_map_zoom; ?>&scale=1&size=600x300&maptype=roadmap&format=png&visual_refresh=true&key=<?php echo $googleMapsStaticApiKey; ?>&signature=<?php echo $googleMapsStaticUrlSigningSecret; ?>" alt="Click to activate map"></a>
 </div>
